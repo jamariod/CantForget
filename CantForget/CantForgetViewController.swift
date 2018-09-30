c88b02dec66801ea62d534d21d60df198fab634b
Jamario Davis <jamariodavis25@gmail.com>
2018-09-30 15:25:09 -0400

    Add new item functionality complete.

--- CantForget/CantForgetViewController.swift
+++ CantForget/CantForgetViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class CantForgetViewController: UITableViewController {
 
-    let itemArray = ["Find Mike", "Buy Eggs", "Get Milk"]
+    var itemArray = ["Find Mike", "Buy Eggs", "Get Milk"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,9 +45,36 @@ class CantForgetViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        
+        let alert = UIAlertController(title: "Add New CantForget Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks the Add Item button on our UIAlert
+           
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+        
+        }
+        
+        alert.addTextField  { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    
+    }
+
 }
-
-
 
 
 

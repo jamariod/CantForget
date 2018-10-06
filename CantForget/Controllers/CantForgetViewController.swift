507d28b7b7c865a0890face49949f6a4bc6b49f6
Jamario Davis <jamariodavis25@gmail.com>
2018-10-06 17:47:47 -0400

    Persisting data using Codable protocol and Plist.

--- CantForget/Controllers/CantForgetViewController.swift
+++ CantForget/Controllers/CantForgetViewController.swift
@@ -11,28 +11,16 @@ import UIKit
 class CantForgetViewController: UITableViewController {
 
     var itemArray = [Item]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
-    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem = Item()
-        newItem.title = "Find Mike"
-       
-        itemArray.append(newItem)
+        print(dataFilePath)
         
-        let newItem2 = Item()
-        newItem2.title = "Buy Eggos"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Destroy Demogorgon"
-        itemArray.append(newItem3)
-        
-        if let items = defaults.array(forKey: "CantForgetListArray") as? [Item] {
-            itemArray = items
-        }
+        loadItems()
+
     }
     
     //MARK -Tableview Datasource Methods
@@ -64,9 +52,7 @@ class CantForgetViewController: UITableViewController {
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done 
         
-      
-        
-        tableView.reloadData()
+        saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -84,11 +70,10 @@ class CantForgetViewController: UITableViewController {
            
             let newItem = Item()
             newItem.title = textField.text! 
+           
             self.itemArray.append(newItem)
             
-            self.defaults.set(self.itemArray, forKey: "CantForgetListArray")
-            
-            self.tableView.reloadData()
+            self.saveItems()
         
         }
         
@@ -102,10 +87,36 @@ class CantForgetViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
     
     }
+    
+    //MARK - Model Manpulation Methods
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        
+        self.tableView.reloadData()
+        
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+            itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
+    
+    }
 
 }
-
-
 
 
 
